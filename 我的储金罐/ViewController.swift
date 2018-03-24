9367e6e08093a482df2907342f0c3a261f5b6c5f
Runkai Zhang <Nico@kens-macbook-pro.local>
2018-03-24 12:29:25 +0800

    Initial Commit

---
+++ 我的储金罐/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  我的储金罐
+//
+//  Created by Runkai Ken Nico Zhang on 3/24/18.
+//  Copyright © 2018 Runkai Ken Nico Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
